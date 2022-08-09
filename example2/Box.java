7cff8720df23424f28d933ca93cbb568bf3daf3b
Hakan DERELİ <hakandereli28@gmail.com>
2022-08-09 23:55:33 +0300

    ilk örnekler

---
+++ example2/Box.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Box {
+
+    private Object myObject;
+
+    public Object getMyObject() {
+        return myObject;
+    }
+
+    public void setMyObject(Object myObject) {
+        this.myObject = myObject;
+    }
+    
+}
